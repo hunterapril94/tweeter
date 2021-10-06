5bc2b341ce74da45d210143fcdeb635aab327d51
April Larsen <hunterapril94@gmail.com>
2021-10-06 03:06:09 +0000

    refactor character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,11 @@
 $(document).ready(function() {
   const text = $(".new-tweet form textarea")
-  let val = $('output').val()
-  text.on('keypress', (e) => {
-    let val = $('output').val()
-    $('output').val(val - 1)
+  text.on('keydown', () => {
+    $('output').val(140 - text.val().length)
 
-    if (val < 0) {
-      $('output').addClass('overlimit')
+    if($('output').val() == 0) {
+      $('output').toggleClass('overlimit')
     }
 
   })
-  text.on("keydown", (e) => {
-    let val = $('output').val()
-    if(e.keyCode === 8 && val < 140) {
-      
-      $('output').val(Number(val) + 1)
-    }
-  })
-
 });

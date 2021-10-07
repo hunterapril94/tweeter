dac1405da26b6a40ce7a4f178d3945b9330b5576
April Larsen <hunterapril94@gmail.com>
2021-10-07 03:07:54 +0000

    add post request at form submission and get request at page load

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -26,29 +26,19 @@ $('document').ready(function(){
       $('#tweets-container').append(newTweet)
     }
   }
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  renderTweets(data);
+
+  $('.new-tweet form').submit(function(e){
+    e.preventDefault();
+    const $newData = $(this).serialize();
+    $.post("/tweets", $newData, function(data){
+      console.log($newData)
+    })
+  });
+
+  const loadTweets = function(){
+    $.get("/tweets", function(data, status) {
+      renderTweets(data);
+    })
+  }
+  loadTweets();
 });

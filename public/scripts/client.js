3926b0e0faf284ef535f22c2589236f3520a3811
April Larsen <hunterapril94@gmail.com>
2021-10-08 01:27:35 +0000

    add comments for clarity

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,61 +1,57 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-$('document').ready(function(){
-  $('#error').hide()
+$('document').ready(function() {
+  $('#error').hide();
   const createTweetElement = function(tweet) {
     //makes it so users can't ruin the site with writing scripts into textarea
-    const escape = function (str) {
+    const escape = function(str) {
       let div = document.createElement("div");
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     };
     const $tweet = $(`<article class="tweet"></article>`);
-    const $header = $(`<header><div><img src=${tweet.user.avatars}><p>${tweet.user.name}</p></div><p>${tweet.user.handle}</p></header>`)
-    const $tweetBody = $(`<p>${escape(tweet.content.text)}</p>`)
-    const $footer = $(`<footer><p>${timeago.format(tweet.created_at)}</p><div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div></footer>`)
+    const $header = $(`<header><div><img src=${tweet.user.avatars}><p>${tweet.user.name}</p></div><p>${tweet.user.handle}</p></header>`);
+    const $tweetBody = $(`<p>${escape(tweet.content.text)}</p>`);
+    const $footer = $(`<footer><p>${timeago.format(tweet.created_at)}</p><div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div></footer>`);
 
-    $tweet.append($header)
-    $tweet.append($tweetBody)
-    $tweet.append($footer)
-    return $tweet
-  }
+    $tweet.append($header);
+    $tweet.append($tweetBody);
+    $tweet.append($footer);
+    return $tweet;
+  };
   const renderTweets = function(tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
-    for(let i = 0; i < tweets.length; i++) {
-      const newTweet = createTweetElement(tweets[i])
-      $('#tweets-container').prepend(newTweet)
+    for (let i = 0; i < tweets.length; i++) {
+      const newTweet = createTweetElement(tweets[i]);
+      $('#tweets-container').prepend(newTweet);
     }
-  }
+  };
 
-  $('.new-tweet form').submit(function(e){
+  $('.new-tweet form').submit(function(e) {
     e.preventDefault();
 
     const $newData = $(this).serialize();
-    if($(".new-tweet form textarea").val().length === 0) {
-      $('#error').show()
-      return $('#error').append("Tweet cannot be blank")
+    if ($(".new-tweet form textarea").val().length === 0) {
+      $('#error').show();
+      return $('#error').append("Tweet cannot be blank");
     } else if ($(".new-tweet form textarea").val().length > 140) {
-      $('#error').show()
-      return $('#error').append("Tweet is over character limit. Consider turning it into a novel.")
+      $('#error').show();
+      return $('#error').append("Tweet is over character limit. Consider turning it into a novel.");
     }
-    $.post("/tweets", $newData, function(data){
+    $.post("/tweets", $newData, function(data) {
       $.get("/tweets", function(data, status) {
+        //renders only the most recent tweet
         renderTweets(data.slice(-1));
-      })
-    })
-    $('.new-tweet form textarea').val("")
+      });
+    });
+    $('.new-tweet form textarea').val("");
+    $('.new-tweet form output').val(140);
   });
 
-  const loadTweets = function(){
+  const loadTweets = function() {
     $.get("/tweets", function(data, status) {
       renderTweets(data);
-    })
-  }
+    });
+  };
   loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,15 @@
-$(document).ready(function() {
-  const text = $(".new-tweet form textarea")
+//function to make the output count characters as well as change color when tweet is overlimit
+$('document').ready(function() {
+  const text = $(".new-tweet form textarea");
   text.on('keyup', () => {
-    $('#error').hide()
-    $('output').val(140 - text.val().length)
+    $('#error').hide();
+    $('output').val(140 - text.val().length);
 
-    if($('output').val() == 0) {
-      $('output').toggleClass('overlimit')
+    if ($('output').val() < 0) {
+      $('output').addClass('overlimit');
+    } else if ($('output').val() >= 0 && $('output.overlimit')) {
+      $('output').removeClass('overlimit');
     }
 
-  })
+  });
 });
